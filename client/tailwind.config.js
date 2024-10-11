6b2f9617afff7d1e7fb8d09c5a89500b4bf66f73
terryluciano <tergeek@gmail.com>
2024-10-10 22:38:32 -0400

    Adding toaster to FE

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -24,6 +24,22 @@ export default {
             borderWidth: {
                 1: "1px",
             },
+            animation: {
+                "toast-enter": "slide-in-right 0.2s ease-in-out",
+                "toast-exit": "slide-in-right reverse 0.2s ease-in-out",
+            },
+            keyframes: {
+                "slide-in-right": {
+                    "0%": {
+                        transform: "translateX(100%)",
+                        opacity: 0,
+                    },
+                    "100%": {
+                        transform: "translateX(0)",
+                        opacity: 1,
+                    },
+                },
+            },
         },
     },
     plugins: [],
