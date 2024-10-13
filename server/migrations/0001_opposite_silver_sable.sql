e93a65b758346c7685a233c360f360375b5d835d
P C <tergeek@gmail.com>
2024-10-12 22:05:49 -0400

    Implemented db store for sessions

--- server/migrations/0001_opposite_silver_sable.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" DROP CONSTRAINT "users_username_unique";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN IF EXISTS "username";
--- server/src/utils/drizzleSessionStore.ts
+++ server/src/utils/drizzleSessionStore.ts
@@ -1,39 +1,109 @@
+'use strict';
+
 import session from 'express-session';
+import { sessions } from '../schema';
+import { db } from '..';
+import { eq, lte } from 'drizzle-orm';
 
 class DrizzleSessionStore extends session.Store {
 	constructor() {
 		super();
 	}
 
-	async destroy(sid: string, callback?: (err?: any) => void): void {
+	#deleteExpiredSessions = async () => {
 		try {
-			callback(null);
+			await db
+				.delete(sessions)
+				.where(lte(sessions.expiresAt, new Date()));
+
+			return;
 		} catch (e) {
-			return callback(e);
+			console.error(e);
+			return;
 		}
-	}
+	};
 
 	async set(
 		sid: string,
 		session: session.SessionData,
 		callback?: (err?: any) => void
-	): void {
+	): Promise<void> {
 		try {
+			await db.insert(sessions).values({
+				sid: sid,
+				data: session,
+				expiresAt: session.cookie.expires,
+			});
+			return callback?.(null);
 		} catch (e) {
-			return callback(e);
+			return callback?.(e);
 		}
 	}
 
 	async get(
 		sid: string,
-		callback: (err: any, session?: session.SessionData | null) => void
-	): void {}
+		callback: (err: any, session?: any) => void
+	): Promise<void> {
+		try {
+			// get session
+			const selectQuery = await db
+				.select()
+				.from(sessions)
+				.where(eq(sessions.sid, sid));
+
+			// check if session exists
+			if (selectQuery.length > 0) {
+				// check if session is expired
+				if (
+					selectQuery[0].expiresAt &&
+					selectQuery[0].expiresAt < new Date()
+				) {
+					// delete session
+					await db.delete(sessions).where(eq(sessions.sid, sid));
+
+					return callback?.(null, null);
+				}
+				return callback?.(null, selectQuery[0].data);
+			} else {
+				return callback?.(null, null);
+			}
+		} catch (e) {
+			return callback?.(e);
+		}
+	}
+
+	async destroy(sid: string, callback?: (err?: any) => void): Promise<void> {
+		try {
+			// delete session
+			await db.delete(sessions).where(eq(sessions.sid, sid));
+
+			return callback?.(null);
+		} catch (e) {
+			return callback?.(e);
+		}
+	}
 
 	async touch(
 		sid: string,
 		session: session.SessionData,
 		callback?: () => void
-	): void {}
+	): Promise<void> {
+		try {
+			// update session
+			await db
+				.update(sessions)
+				.set({ expiresAt: session.cookie.expires })
+				.where(eq(sessions.sid, sid));
+
+			// delete expired sessions
+			this.#deleteExpiredSessions();
+
+			return callback?.();
+		} catch (e) {
+			console.error(e);
+			return callback?.();
+		}
+	}
 }
 
 export default DrizzleSessionStore;
